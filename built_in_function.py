73b769a9c3ec751573176ea01131f324e29586a4
Sri Kant Kumar <srikant4352@gmail.com>
2026-09-19 12:14:58 +0530

    Create built_in_function.py

---
+++ built_in_function.py
@@ -0,0 +1,44 @@
+a ={1,2,3,4}
+a_type=type(a)
+print(a_type)
+
+a ='2'
+a =int(a)
+print(a)
+
+a =pow(2,-1)
+print(a)
+
+a =-3.4
+print(abs(a))
+
+a =[2,3,4,5,0,-1,-4]
+print(max(a))
+print(min(a))
+
+a =22/7
+a =round(a,2)
+print(a)
+
+a =divmod(22,7)
+print(a)
+
+a =bin(5)
+print(a)
+a =oct(5)
+print(a)
+a =hex(5)
+print(a)
+
+c =id(a)
+print(c)
+
+c =ord('A')
+print(c)
+
+print(len("sri kant kumar"))
+
+print(sum([1,2,3,4,56,0.9]))
+
+print(help(print))
+

6c870a07b7e3c484ee1d4aacef7eaf733dd95a08
Sri Kant Kumar <srikant4352@gmail.com>
2026-09-21 16:56:28 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,24 @@
+import math as mt
+import random as rd
+import time as t
+import os as operating_system
+
+
+print(mt.pi)
+print(mt.e)
+print(mt.floor(3.4))
+
+print(rd.randint(1,23))
+list =[1,2,3,4,5]
+rd.shuffle(list)
+print(list)
+
+print(t.time())
+print(t.ctime())
+
+print("hello",end=" ")
+t.sleep(5)
+print("world")
+
+print(operating_system.listdir())
+print(operating_system.getcwd())
